3cffd384da355f0cdd8bdd35714d4a1f659bf1d0
LeeKangWoo <48194683+LeeKangW@users.noreply.github.com>
2021-06-28 11:16:38 +0900

    주석 추가

--- API/Attributes/ReadOnly/ReadOnly.cs
+++ API/Attributes/ReadOnly/ReadOnly.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 public class ReadOnly : PropertyAttribute
 {
+    /// <summary>
+    /// If true, Can Edit before playing Game
+    /// </summary>
     public readonly bool isRunTimeOnly;
 
     public ReadOnly(bool runtimeOnly = false)
