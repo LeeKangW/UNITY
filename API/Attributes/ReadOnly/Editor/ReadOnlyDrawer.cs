ce6d28f0d395d5ea954289653d22d126e56fd89c
LeeKangWoo <48194683+LeeKangW@users.noreply.github.com>
2021-06-28 11:17:58 +0900

    Update ReadOnlyDrawer.cs

--- API/Attributes/ReadOnly/Editor/ReadOnlyDrawer.cs
+++ API/Attributes/ReadOnly/Editor/ReadOnlyDrawer.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEditor;
 
-[CustomPropertyDrawer(typeof(ReadOnly))]
+[CustomPropertyDrawer(typeof(ReadOnly), true)]
 public class ReadOnlyDrawer : PropertyDrawer
 {
     public override bool CanCacheInspectorGUI(SerializedProperty property)
