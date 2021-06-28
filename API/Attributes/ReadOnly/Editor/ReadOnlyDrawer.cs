3c2c375acd5d965fb7fcdd7d56ac7a20f84e9f1a
LeeKangWoo <48194683+LeeKangW@users.noreply.github.com>
2021-06-28 10:19:23 +0900

    Create ReadOnlyDrawer.cs

---
+++ API/Attributes/ReadOnly/Editor/ReadOnlyDrawer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+public class ReadOnlyDrawer : PropertyDrawer
+{
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        return base.GetPropertyHeight(property, label);
+    }
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        GUI.enabled = false;
+        EditorGUI.PropertyField(position, property, label, true);
+        GUI.enabled = true;
+    }
+}
