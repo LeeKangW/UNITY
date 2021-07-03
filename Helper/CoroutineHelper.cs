cb0e18fe0c4e2d09899dec80566f0e5c6ff67233
LeeKangWoo <48194683+LeeKangW@users.noreply.github.com>
2021-07-03 17:01:16 +0900

    Create CoroutineHelper.cs

---
+++ Helper/CoroutineHelper.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class CoroutineHelper
+{
+    public static readonly WaitForSeconds wait0ms = new WaitForSeconds(0f);
+    public static readonly WaitForSeconds wait100ms = new WaitForSeconds(0.1f);
+    public static readonly WaitForSeconds wait500ms = new WaitForSeconds(0.5f);
+    public static readonly WaitForSeconds wait1000ms = new WaitForSeconds(1f);
+   public static IEnumerator WaitUntil(Func<bool> func)
+   {
+        while (!func())
+        {
+            yield return wait0ms;
+        }
+   }
+}
