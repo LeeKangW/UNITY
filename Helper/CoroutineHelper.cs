29f3c5dd79a965d6374de11f9948f0959f219abb
LeeKangWoo <48194683+LeeKangW@users.noreply.github.com>
2021-07-12 12:51:24 +0900

    namespace 추가

--- Helper/CoroutineHelper.cs
+++ Helper/CoroutineHelper.cs
@@ -2,17 +2,21 @@ using System;
 using System.Collections;
 using UnityEngine;
 
-public class CoroutineHelper
+
+namespace Helper
 {
-    public static readonly WaitForSeconds wait0ms = new WaitForSeconds(0f);
-    public static readonly WaitForSeconds wait100ms = new WaitForSeconds(0.1f);
-    public static readonly WaitForSeconds wait500ms = new WaitForSeconds(0.5f);
-    public static readonly WaitForSeconds wait1000ms = new WaitForSeconds(1f);
-   public static IEnumerator WaitUntil(Func<bool> func)
-   {
-        while (!func())
+    public class Coroutine
+    {
+        public static readonly WaitForSeconds wait0ms = new WaitForSeconds(0f);
+        public static readonly WaitForSeconds wait100ms = new WaitForSeconds(0.1f);
+        public static readonly WaitForSeconds wait500ms = new WaitForSeconds(0.5f);
+        public static readonly WaitForSeconds wait1000ms = new WaitForSeconds(1f);
+        public static IEnumerator WaitUntil(Func<bool> func)
         {
-            yield return wait0ms;
+            while (!func())
+            {
+                yield return wait0ms;
+            }
         }
-   }
+    }
 }
